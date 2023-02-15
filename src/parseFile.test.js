2cb818ed88641d3f267fd0bf293945c2bfb75d21
Ed Thome <ethome@thoughtworks.com>
2023-02-15 11:18:10 -0600

    Parameterize file parser tests

--- src/parseFile.test.js
+++ src/parseFile.test.js
@@ -18,47 +18,52 @@ describe("parseFile", () => {
     mockFs.restore();
   });
 
-  it("should read input file and return plateau with specified upper right coordinates", async () => {
-    mockFile("2 3");
-
-    expect(await parseFile(FILEPATH)).toEqual({
-      plateau: { width: 3, height: 4 },
-      rovers: [],
-    });
-  });
-
-  it("should parse rover with instructions", async () => {
-    mockFile("4 5\n1 1 S\nLMR");
-
-    expect(await parseFile(FILEPATH)).toEqual({
-      plateau: { width: 5, height: 6 },
-      rovers: [
-        {
-          position: { x: 1, y: 1 },
-          direction: SOUTH,
-          instructions: [TURN_LEFT, MOVE, TURN_RIGHT],
+  describe("parsing file", () => {
+    it.each([
+      {
+        scenario: "plateau with specified upper right coordinates",
+        input: "2 3",
+        output: {
+          plateau: { width: 3, height: 4 },
+          rovers: [],
         },
-      ],
-    });
-  });
-
-  it("should parse two rovers", async () => {
-    mockFile("10 12\n0 4 E\nLLLL\n10 12 W\nMR");
-
-    expect(await parseFile(FILEPATH)).toEqual({
-      plateau: { width: 11, height: 13 },
-      rovers: [
-        {
-          position: { x: 0, y: 4 },
-          direction: EAST,
-          instructions: [TURN_LEFT, TURN_LEFT, TURN_LEFT, TURN_LEFT],
+      },
+      {
+        scenario: "rover with instructions",
+        input: "4 5\n1 1 S\nLMR",
+        output: {
+          plateau: { width: 5, height: 6 },
+          rovers: [
+            {
+              position: { x: 1, y: 1 },
+              direction: SOUTH,
+              instructions: [TURN_LEFT, MOVE, TURN_RIGHT],
+            },
+          ],
         },
-        {
-          position: { x: 10, y: 12 },
-          direction: WEST,
-          instructions: [MOVE, TURN_RIGHT],
+      },
+      {
+        scenario: "two rovers",
+        input: "10 12\n0 4 E\nLLLL\n10 12 W\nMR",
+        output: {
+          plateau: { width: 11, height: 13 },
+          rovers: [
+            {
+              position: { x: 0, y: 4 },
+              direction: EAST,
+              instructions: [TURN_LEFT, TURN_LEFT, TURN_LEFT, TURN_LEFT],
+            },
+            {
+              position: { x: 10, y: 12 },
+              direction: WEST,
+              instructions: [MOVE, TURN_RIGHT],
+            },
+          ],
         },
-      ],
+      },
+    ])("should parse $scenario", async ({ input, output }) => {
+      mockFile(input);
+      expect(await parseFile(FILEPATH)).toEqual(output);
     });
   });
 
